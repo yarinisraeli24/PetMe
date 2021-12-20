d70176cf8408e32598cfe96f89e9e29ef45eea4d
Yarin Israeli <i558206@c02f42esmd6t.lan>
2021-12-20 17:47:06 +0200

    Add navbar and make card with swiping animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
+import React , {useState, useEffect} from 'react';
+import Button from '@mui/material/Button';
+import { CardSwiper } from "react-card-rotate-swiper";
+import Navbar from './Navbar';
+import Card from './Card'
 import './App.css';
 
+
 function App() {
+  const images = ['https://thediscerningcat.com/wp-content/uploads/2021/09/british-short-hair-chincilla-up-close.jpg.webp','https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=980:*']
+  console.log('rendered')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <Navbar />
+      <div className="card">
+        {images.map((imageUrl, index) => <Card zIndex={index} image={imageUrl}/>)}
+      </div>
     </div>
   );
 }
